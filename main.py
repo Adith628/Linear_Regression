d977b46fa2faf5f81f29fa50533dc8eb7b4f7d1b
Adithyan T <adithsreedhar628@gmail.com>
2023-05-30 11:00:11 +0530

    splitting data into test and training

--- main.py
+++ main.py
@@ -18,3 +18,13 @@ labelencoder = LabelEncoder()
 x[:, 3] =labelencoder.fit_transform(x[:, 3])
 ct = ColumnTransformer([('one_hot_encoder', OneHotEncoder(), [3])], remainder='passthrough')
 x = ct.fit_transform(x)
+
+# Removing dummy variable trap
+x = x[:, 1:]
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import reain_test_spilt 
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state = 0)
+
+# Fitting multiple linear regression to the training set
+
